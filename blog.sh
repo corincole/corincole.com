45c751ca064c668fd67cd597ebfc548dd3a42955
Corin Cole <corin.c.cole@gmail.com>
2012-01-15 23:56:16 +0000

    shell script for site generation

--- blog.sh
+++ blog.sh
@@ -1,1 +1,4 @@
-/root/blog.sh
+rm -rf /var/www/_sites/corin/dev.corincole.com/*
+jekyll /home/corin/jekyll/corincole.com/ /var/www/_sites/corin/dev.corincole.com/
+ln -s /var/www/_sites/corin/misc/ /var/www/_sites/corin/dev.corincole.com/misc
+s3cmd -r -P --add-header 'Cache-Control: max-age=300' sync /var/www/_sites/corin/dev.corincole.com/ s3://corinsite/
